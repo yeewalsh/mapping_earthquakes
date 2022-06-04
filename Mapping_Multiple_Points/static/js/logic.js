ad2ba5423da6fdc1400c019bdf61a1dface03bf7
Ellye Walsh <ellyewalsh@gmail.com>
2022-06-04 13:33:46 -0400

    Adding multiple points

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -31,7 +31,13 @@ console.log(cityData);
 // loop through cities array and create one marker for each city
 cityData.forEach(function(city) {
     console.log(city)
-    L.marker(city.location).addTo(map);
+    L.circleMarker(city.location, {
+        radius: city.population/100000,
+        color: "orange",
+        lineweight: 4
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
 });
 
 // cityData.forEach(function (city) { 
